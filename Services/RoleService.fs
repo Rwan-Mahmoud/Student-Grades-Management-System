a1f83b61f8076b3ca8618ce581960062dadf2024
Ahmed Ibrahim <Ahmedelgamal0596@gmail.com>
2025-12-11 22:31:17 +0200

    Add RoleService module with admin password validation

--- Services/RoleService.fs
+++ Services/RoleService.fs
@@ -1,1 +1,11 @@
-hiii ahmed
+namespace StudentGradesV7.Services
+open StudentGradesV7.Models
+
+module RoleService =
+
+//! isValidAdminPasswor (pure)
+    let isValidAdminPassword (password: string) : bool =
+        password = StudentGradesV7.Models.Data.adminPassword
+
+    let logout () = Viewer
+
